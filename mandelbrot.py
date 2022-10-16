251b5f409cf71758ea1c2e4acd538dbefea77d35
megham1nd <megha.rao@stonybrook.edu>
2022-10-16 19:47:17 -0400

    Adding comments and documentation

--- mandelbrot.py
+++ mandelbrot.py
@@ -4,20 +4,40 @@ import matplotlib.pyplot as plt
 np.warnings.filterwarnings("ignore")  # type: ignore
 
 def mandelbrot_plot(n, N, thresh):
+    '''
+    Returns computed Mandelbrot set within an nxn grid in the range
+    [-2, 1] x [-1.5i, 1.5i]. 
+    The divergence test |z| < thresh is run on each point in the grid
+    following N iterations of the Mandelbrot function.
+    '''
     x = np.linspace(-2, 1, n)
     y = np.linspace(-1.5, 1.5, n)
 
+    # creating the grid of points on which to run the Mandelbrot
+    # function
     grid_x, grid_y = np.meshgrid(x, y)
 
-    mask = mandelbrot_func(grid_x, grid_y, N)
-    mb_set = np.zeros([n, n])
-    mb_set[np.abs(mask) < thresh] = 1   
+    # running the Mandelbrot function on each point in the grid
+    mb_set = mandelbrot_func(grid_x, grid_y, N)
+    # applying a binary mask on the resulting set depending on 
+    # the threshold condition |z| < thresh:
+    # 1 if the point belongs in the set
+    # 0 if the point does not belong in the set
+    mask = np.zeros([n, n])
+    mask[np.abs(mb_set) < thresh] = 1   
 
+    # plotting the resulting set
     plt.imshow(np.abs(mb_set), extent = [-2, 1, -1.5, 1.5])
     plt.gray()
     plt.savefig('mandelbrot.png')
 
 def mandelbrot_func(x, y, N):
+    '''
+    Returns product (z) of Mandelbrot function according to input 
+    variables x and y after N iterations:
+
+    z = z^2 + (x + yi)
+    '''
     z = 0
     c = x + 1j*y
 
--- markov_chain.py
+++ markov_chain.py
@@ -3,31 +3,51 @@ import scipy as sci
 import matplotlib.pyplot as plt
 
 def norm_random_vec(n):
+    '''
+    Returns a normalized random n-vector.
+    '''
     vector = np.random.rand(1, n)[0]
     return vector/np.sum(vector)
 
 def norm_random_matrix(n):
-     matrix = np.random.rand(n, n)
-     mat_sums = np.sum(matrix, axis=1)
+    '''
+    Returns a normalized random nxn matrix.
+    '''
+    matrix = np.random.rand(n, n)
+    mat_sums = np.sum(matrix, axis=1)
 
-     return matrix/mat_sums.reshape(n,1)
+    return matrix/mat_sums.T
 
 def markov_chain(n, N):
-    p = norm_random_vec(n)
-    P = norm_random_matrix(n)
+    '''
+    Simulating N steps of a Markov chain with n states. 
 
+    This function starts begins with a random probability distribution
+    between states, and transitions between states according to a 
+    randomly generated transition matrix.
+    '''
+
+    p = norm_random_vec(n)      # random intial probability distribution
+    P = norm_random_matrix(n)   # random transition matrix
+
+    # computing the eigenvector with the largest associated eigenvalue
+    # lambda = 1
     values, vectors = (np.linalg.eig(P.T))
     p_stationary = vectors[:, np.argmax(values)]
     p_stationary = p_stationary/np.sum(p_stationary)
 
     norms = [np.linalg.norm(p-p_stationary)]
 
+    # stepping through N iterations of the Markov chain
     for i in range(N):
-        p = np.matmul(P.T, p)
+        p = np.matmul(P.T, p)   # updating current probability distribution between states
+        # recording the normalized difference p and p_stationary
+        # for each step
         norms.append(np.linalg.norm(p-p_stationary))
 
+    # plotting the normalized difference between p and p_stationary
+    # against the number of steps/iterations of the Markov chain
     plt.plot(norms)
     plt.savefig('markov_chain_{}_{}.png'.format(n, N))
 
-norm_random_matrix(3)
 markov_chain(5, 10)
